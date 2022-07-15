80484696cd760cd93817313ba024e01f4f1c5dce
c.winger <c.winger@oeko.de>
2022-07-15 16:04:35 +0200

    improved test

--- test/test_example.py
+++ test/test_example.py
@@ -7,25 +7,31 @@ import unittest
 import scapi.build
 
 logging.basicConfig(
-    format="[%(asctime)s %(levelname)7s] %(message)s", level=logging.DEBUG
+    format="[%(asctime)s %(levelname)7s] %(message)s", level=logging.INFO
 )
 
 
 class TestExample(unittest.TestCase):
     def test_example(self):
         # build example api
-        output_dir = tempfile.TemporaryDirectory("scapi")
-        logging.debug(f"output_path: {output_dir}")
-        example_dir = os.path.join(os.path.dirname(__file__), "example")
-        schema_json = os.path.join(example_dir, "example_schema.json")
+        with tempfile.TemporaryDirectory("scapi") as output_dir:
+            logging.debug(f"output_path: {output_dir}")
+            example_dir = os.path.join(os.path.dirname(__file__), "example")
+            schema_json = os.path.join(example_dir, "example_schema.json")
 
-        # build the api
-        scapi.build.build(schema_json, output_dir.name)
+            # build the api
+            scapi.build.build(schema_json, output_dir)
 
-        # run unittest of created package
-        proc = sp.Popen(["python3", f"{output_dir.name}/test.py"], shell=True)
-        proc.communicate()
+            # run unittest of created package
+            env = os.environ
+            env["PYTHONPATH"] = example_dir
+            proc = sp.Popen(
+                ["python3", f"{output_dir}/test.py"],
+                shell=True,
+                env=env,
+                stdout=sp.PIPE,
+                stderr=sp.PIPE,
+            )
+            proc.communicate()
 
-        self.assertEqual(proc.returncode, 0)
-
-        output_dir.cleanup()
+            self.assertEqual(proc.returncode, 0)
